babcbf433686f2625dfbeae7c0ef3f3e51087bd5
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-26 23:50:58 +0530

    added new challenge

---
+++ src/interview-questions/infinite-scroll/InfiniteScroll.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InfiniteScroll = () => {
+  return (
+    <View>
+      <Text>InfiniteScroll</Text>
+    </View>
+  )
+}
+
+export default InfiniteScroll
+
+const styles = StyleSheet.create({})

83b8c68a9b0f870ca106b2021deef9f592b1c078
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-04-12 22:55:43 +0530

    minor changes in flatten object

--- src/interview-questions/object-mastery/object-flattening/flattenObj.js
+++ src/interview-questions/object-mastery/object-flattening/flattenObj.js
@@ -27,5 +27,12 @@ const flattenObj = (obj) => {
     return result;
 }
 
+// Step 5: Inside Array Q
+// Array behaves like:
+// {
+//   0: 1,
+//   1: 2
+// }
+
 console.log(flattenObj(obj));
 // { A: '12', B: 23, 'C.P': 23, 'C.O.L': 56, 'C.Q.0': 1, 'C.Q.1': 2 }
